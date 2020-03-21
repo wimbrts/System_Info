7dea2e2ca76068866f5ad3fc9467ab61cac731c6
wimb <38395846+wimbrts@users.noreply.github.com>
2020-03-21 08:05:47 +0100

    Add files via upload

---
+++ makebt/listusbdrives/ListUsbDrives_Win32_To_Notepad.cmd
@@ -0,0 +1,2 @@
+Win32\ListUsbDrives.exe -a > %temp%\ListDrives_Win32.txt
+start %temp%\ListDrives_Win32.txt
---
+++ makebt/listusbdrives/ListUsbDrives_x64_To_Notepad.cmd
@@ -0,0 +1,2 @@
+x64\ListUsbDrives.exe -a > %temp%\ListDrives_x64.txt
+start %temp%\ListDrives_x64.txt
